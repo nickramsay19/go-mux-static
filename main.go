1a637980347a0d1f81198bd03a3a1149f5402b56
Nicholas Ramsay <nick@yousponsor.me>
2020-09-01 22:58:12 +1000

    adding files

--- main.go
+++ main.go
@@ -2,36 +2,14 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"log"
+
 	"net/http"
 	"github.com/gorilla/mux"
 )
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to my simple website!\n")
-}
-
-func AddHandler(w http.ResponseWriter, r *http.Request) {
-
-	// get url variables
-    vars := mux.Vars(r)
-    num1, err1 := strconv.Atoi(vars["num1"])
-	num2, err2 := strconv.Atoi(vars["num2"])
-	
-	// check for error
-	if err1 != nil || err2 != nil {
-		fmt.Fprintf(w, "Sorry, there was an error. :(")
-	} else {
-		fmt.Fprintf(w, "%d\n", num1 + num2)
-	}
-}
-
 func main() {
-	r := mux.NewRouter()
-	
-    r.HandleFunc("/", HomeHandler)
-	r.HandleFunc("/add/{num1}/{num2}", AddHandler)
+	r := NewRouter()
 	
 	http.Handle("/", r)
 	
@@ -39,4 +17,18 @@ func main() {
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		log.Fatal("ListenAndServe Error: ", err)
 	}
+}
+
+func NewRouter() *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+
+	// Choose the folder to serve
+	staticDir := "/"
+
+	// Create the route
+	router.
+		PathPrefix(staticDir).
+		Handler(http.StripPrefix(staticDir, http.FileServer(http.Dir("."+staticDir))))
+
+	return router
 }
